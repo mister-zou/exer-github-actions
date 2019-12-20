b564dd57ceaaa23b580fd5e275e4ca5b7e9e4d35
zouxin <mister.zou@foxmail.com>
2019-12-20 19:40:12 +0800

    添加代码

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ mod tests {
     fn larger_can_hold_smaller() {
         let larger = Rectangle { width: 8, height: 7 };
         let smaller = Rectangle { width: 5, height: 1 };
-
+        println!("this is a test");
         assert!(larger.can_hold(&smaller));
     }
 }
